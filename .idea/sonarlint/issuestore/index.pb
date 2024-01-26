74ec75f8ae12921a5f9b53ff2609081bb6e886b9
Rokas <rookas.rudzenskas@gmail.com>
2024-01-26 18:18:27 +0200

    fix

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,13 +3,9 @@ G
 api/stepzen.config.json,9/0/90f204aced47456a8c33936ce713fe087a6551a6
 K
 api/exercises/index.graphql,b/b/bb02f943bc1eede882b17831c4fe0fe38c18fc48
-4
-.env,3/c/3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
 I
 api/curl-01/index.graphql,d/b/db5f6d24be73f7e27b75aa5b2616f1158c726e74
 I
 api/mongodb/index.graphql,1/a/1abfe89cf4597a8bcd29299b4fa49eff7d0c5a27
-C
-src/app/_layout.jsx,b/c/bcc79ed1cfb91a9632bfc7cf230d2d9f461fae68
 B
 src/app/[name].jsx,5/4/5475eae09eb7191cb1e5cee072701fb4c64e1d96
--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,12 +1,23 @@
 import { Stack } from 'expo-router';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Platform } from 'react-native';
 
 import AuthContextProvider from '../providers/AuthContext';
+import {useEffect} from "react";
+import Purchases from "react-native-purchases";
 
 const client = new QueryClient();
 
 export default function RootLayout() {
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      Purchases.configure({apiKey: ""});
+    } else if (Platform.OS === 'android') {
+      Purchases.configure({apiKey: ""});
+    }
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <AuthContextProvider>
